a811e9a8209d5f6cc571db1922b5f1d9d9fbaa77
LipCam <lipcam@hotmail.com.br>
2023-09-11 08:56:32 -0300

    Add EntityFramework e conexao ao banco

---
+++ ClinicaApiCore/DB/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ClinicaApiCore.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClinicaApiCore.DB
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        #region ----- Medicos -----
+        public DbSet<Medicos> Medicos { get; set; }
+        #endregion
+    }
+}
